32f84b7f9627df2c0db360cfb5017fa8256fc53a
Eren <erenburukcu75@gmail.com>
2024-09-05 21:50:42 +0300

    proje2

---
+++ proje2/code.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    });
+
+    const paragraphs = document.querySelectorAll('.Planets div');
+    paragraphs.forEach(paragraph => {
+        observer.observe(paragraph);
+    });
+});
+
+document.querySelectorAll('nav.tags a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
