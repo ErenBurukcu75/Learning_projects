11c2b1fa6647730b15b3ea4fd1ea6963b26a30c2
Eren <erenburukcu75@gmail.com>
2024-09-16 16:36:17 +0300

    new

--- sapui5_proje2/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["sapui5proje2/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- sapui5_proje2/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"sapui5_proje2/test/unit/controller/List.controller"
-], function () {
-	"use strict";
-});
